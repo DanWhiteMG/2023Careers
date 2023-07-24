4cdd3b0dc539e6d374877cbb92089503d7863a83
DanWhiteMG <124805539+DanWhiteMG@users.noreply.github.com>
2023-07-24 10:54:51 +0100

    Update job-search.js

--- job-search.js
+++ job-search.js
@@ -106,7 +106,7 @@ function printArrayToDOM(array) {
            
 		
 	// amend date format to UK date format for Last Updated Date
-		console.log('Hello');		
+		console.log('Hello ' + crfd0_lastchanged@OData.Community.Display.V1.FormattedValue + " is the date');		
             let lastUpdated = stringToDate(array[i].crfd0_recruitingstartdate, "dd/mm/yyyy", "/"); //Updated
             lastUpdated = lastUpdated.toLocaleDateString(lastUpdated);
             jobDate.textContent = `Recruiting Start Date: ${lastUpdated}`; //Updated
