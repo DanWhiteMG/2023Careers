f3dc9cef99b4419254e7c30616e025f821c5bcf7
DanWhiteMG <124805539+DanWhiteMG@users.noreply.github.com>
2023-07-24 10:30:20 +0100

    Update job-search.js

--- job-search.js
+++ job-search.js
@@ -107,7 +107,7 @@ function printArrayToDOM(array) {
 			
 			
 			
-            let lastUpdated = stringToDate(array[i].crfd0_recruitingstartdate@OData.Community.Display.V1.FormattedValue, "dd-mm-yyyy", "-"); //Updated
+            let lastUpdated = stringToDate(array[i].crfd0_recruitingstartdate, "dd-mm-yyyy", "-"); //Updated
             lastUpdated = lastUpdated.toLocaleDateString(lastUpdated);
             jobDate.textContent = `Recruiting Start Date: ${lastUpdated}`; //Updated
             // append content to parent elements
